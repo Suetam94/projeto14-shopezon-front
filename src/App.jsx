931828fe3cf46e02f7244b642655e907d0fca01e
Renan Araujo <ra.carvalho@unesp.br>
2022-07-07 14:44:18 -0300

    creating route, inputs, validations and layout temporaly for the login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import SignUpPage from './pages/SignUp/index.jsx'
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignUpPage from "./pages/SignUp/index";
+import SignInPage from "./pages/SignIn/index";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/cadastrar' element={<SignUpPage/>}/>
+        <Route path="/cadastrar" element={<SignUpPage/>}/>
+        <Route path="/login" element={<SignInPage/>}/>
       </Routes>
     </BrowserRouter>
   )
