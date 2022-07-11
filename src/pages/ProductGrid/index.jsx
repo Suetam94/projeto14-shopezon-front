93176c524b38f301c8cb584ccc38efcba04cafaf
Mateus Vinicius da Silva <mateusviniciusdasilva@outlook.com>
2022-07-11 15:54:38 -0300

    consuming product get all api

--- src/pages/ProductGrid/index.jsx
+++ src/pages/ProductGrid/index.jsx
@@ -5,25 +5,34 @@ import {
   ProductGridCards,
 } from "./productGridStyle";
 import { ProductCard } from "../ProductCard";
+import { useEffect, useState } from "react";
+import { app } from "../../api/axios";
 
 export function ProductGrid() {
+  const [productData, setProductData] = useState([]);
+
+  useEffect(() => {
+    async function getProductData() {
+      const request = await app.get("/products/all");
+
+      if (request.status === 200) {
+        setProductData(request.data);
+      }
+    }
+
+    getProductData();
+  }, []);
+
+  console.log(productData);
+
   return (
     <ProductGridContainer>
       <ProductGridContent>
         <ProductGridTitles>Produtos disponíveis:</ProductGridTitles>
         <ProductGridCards>
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          {productData.map((product, index) => {
+            return <ProductCard key={index} productData={product} />;
+          })}
         </ProductGridCards>
       </ProductGridContent>
     </ProductGridContainer>
